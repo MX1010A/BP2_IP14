85010a075f8d23ec8035d5a35965545c21e7965a
ІP-14 Anton Basan <89969100+MX1010A@users.noreply.github.com>
2022-03-15 21:36:07 +0200

    Add files via upload

---
+++ Lab2/Lab2/Lab2/OfficeAppliances.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static Lab2.Func;
+namespace Lab2
+{
+    [Serializable]
+    public struct OfficeAppliances
+    {
+        public OfficeAppliances(string name, string dateOfPurchase, double warrantyTerm)
+        {
+            Name = name;
+            DateOfPurchase = dateOfPurchase;
+            WarrantyTerm = warrantyTerm;
+        }
+        
+        private string Name {get; set;}
+        private string DateOfPurchase {get; set;}
+        private double WarrantyTerm {get; set;}
+        
+        public bool IsWarrantyExpired()
+        {
+            bool isExpired = false;
+            DateTime date1 = DateTime.Parse(DateOfPurchase);
+            DateTime date2 = date1.AddDays(WarrantyTerm);
+            if (date2 < DateTime.Now) isExpired = true;
+            return isExpired;
+        }
+
+        public string String() => $"{Name}{Tab(Name.Length)}purchased: {DateOfPurchase}\twarranty: {WarrantyTerm} days";
+    }
+}
