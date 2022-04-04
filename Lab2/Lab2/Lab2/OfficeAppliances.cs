3541d87be913c4a1a8ee47b8ecfe247cf22e6f98
ІP-14 Anton Basan <89969100+MX1010A@users.noreply.github.com>
2022-04-04 14:28:05 +0200

    Add files via upload

---
+++ Lab2/Lab2/Lab2/OfficeAppliances.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static Lab2.Func;
+namespace Lab2
+{
+    [Serializable]
+    public struct OfficeAppliances
+    {
+        public OfficeAppliances(string name, string dateOfPurchase, double warrantyTerm) //конструктор
+        {
+            Name = name;
+            DateOfPurchase = dateOfPurchase;
+            WarrantyTerm = warrantyTerm;
+        }
+        
+        private string Name {get; set;}
+        private string DateOfPurchase {get; set;}
+        private double WarrantyTerm {get; set;}
+        
+        //перевірка на дійсність гарантії
+        public bool IsWarrantyExpired()
+        {
+            bool isExpired = false;
+            DateTime date1 = DateTime.Parse(DateOfPurchase);
+            DateTime date2 = date1.AddDays(WarrantyTerm);
+            if (date2 < DateTime.Now) isExpired = true;
+            return isExpired;
+        }
+        
+        //конвертування інформації про техніку в рядок
+        public string String() => $"{Name}{Tab(Name.Length)}purchased: {DateOfPurchase}\twarranty: {WarrantyTerm} days";
+    }
+}
