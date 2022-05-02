f538d289554fa15e47f660a407c470afc1909475
ІP-14 Anton Basan <89969100+MX1010A@users.noreply.github.com>
2022-05-02 21:13:37 +0200

    Lab4

---
+++ Lab4/Lab4/Lab4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab4
+{
+  internal static class Program
+  {
+    public static void Main(string[] args)
+    {
+      Time t1 = new Time(4, 20, 28);
+      Time t2 = new Time();
+      
+      Console.WriteLine($"\nT1: {t1.Hours()}:{t1.Minutes()}:{t1.Seconds()}");
+      Console.WriteLine($"T2: {t2.Hours()}:{t2.Minutes()}:{t2.Seconds()}");
+      
+      int m1 = 17, m2 = 34;
+      t1 += m1;
+      t2 += m2;
+      
+      Console.WriteLine($"\nT1 after adding 17 m: {t1.Hours()}:{t1.Minutes()}:{t1.Seconds()}");
+      Console.WriteLine($"T2 after adding 34 m: {t2.Hours()}:{t2.Minutes()}:{t2.Seconds()}");
+      Console.WriteLine($"Time between T1 & T2: {(t1 - t2).Hours()}:{(t1 - t2).Minutes()}:{(t1 - t2).Seconds()}");
+      
+      Time t3 = new Time(t1);
+      
+      Console.WriteLine($"\nT3: {t3.Hours()}:{t3.Minutes()}:{t3.Seconds()}");
+      Console.WriteLine($"Time left until the end of day: {t3.ToEod().Hours()}:{t3.ToEod().Minutes()}:{t3.ToEod().Seconds()}");
+    }
+  }
+}
---
+++ Lab4/Lab4/Lab4/Time.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Lab4
+{
+    public class Time
+    {
+        private readonly int hours, minutes, seconds;
+        
+        public Time()
+        {
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        }
+        
+        public Time(int hours, int minutes = 0, int seconds = 0)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+        }
+        
+        public Time(Time protoTime)
+        {
+            hours = protoTime.hours;
+            minutes = protoTime.minutes;
+            seconds = protoTime.seconds;
+        }
+
+        public int Hours() => hours;
+
+        public int Minutes() => minutes;
+
+        public int Seconds() => seconds;
+
+        public Time ToEod() => new Time(23 - hours, 59 - minutes, 60 - seconds);
+        
+        public static Time operator +(Time currT, int minutes)
+        {
+            int hours = 0;
+            int m = minutes;
+            m += 2;
+            if (m >= 60)
+            {
+                hours++;
+                minutes -= 60;
+            }
+            return new Time(currT.hours + hours, currT.minutes + minutes, currT.seconds);
+        }
+        
+        public static Time operator -(Time t1, Time t2)
+        {
+            DateTime dt1 = DateTime.Parse($"{t1.hours}:{t1.minutes}:{t1.seconds}");
+            DateTime dt2 = DateTime.Parse($"{t2.hours}:{t2.minutes}:{t2.seconds}");
+            return new Time((dt1 - dt2).Hours, (dt1 - dt2).Minutes, (dt1 - dt2).Seconds);
+        }
+    }
+}
