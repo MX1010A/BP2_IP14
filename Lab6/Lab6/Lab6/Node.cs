cd70de042c927fcc08f02d84f01e40517ff31716
ІP-14 Anton Basan <89969100+MX1010A@users.noreply.github.com>
2022-06-05 22:11:38 +0200

    Lab6

---
+++ Lab6/Lab6/Lab6/Node.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+
+namespace Lab6
+{
+    public class Node
+    {
+        public Node(char value)
+        {
+            Value = value;
+        }
+        
+        private char Value { get; }
+        public Node? Left { get; set; }
+        public Node? Right { get; set; }
+        public override string ToString() => $"{Value}";
+    }
+}
---
+++ Lab6/Lab6/Lab6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static Lab6.Func;
+
+namespace Lab6
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = "(a*(b+c))/d";
+            Node root = GenerateTree(expression);
+            Console.WriteLine($"\nBinary tree for {expression}:");
+            Console.WriteLine($"Level #1: {root}\n{String(root, 2)}");
+        }
+    }
+}
